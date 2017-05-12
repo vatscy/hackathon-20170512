5e922a8fa313bd70da0db5c1ba2261f85a8e8486
Vatscy <vatscy8241@gmail.com>
2017-05-12 16:14:44 +0900

    データバインディング途中

--- frontend/scripts/detail.js
+++ frontend/scripts/detail.js
@@ -3,17 +3,15 @@ $(function() {
     type: 'GET',
     url: 'https://gxl6xlv440.execute-api.ap-northeast-1.amazonaws.com/dev/toilet',
     data: {
-      id: 123456789
+      id: 1494570067
     },
     dataType: 'json'
   })
   .done(function (data) {
     document.title = data.name + ' - 新宿区/ [トイレの食べログ]';
     $('#toilet-name').text(data.name);
-
-    for (var i = 0, len = data.photo_catch.length; i < len; i++) {
-      $('#mainphoto-view').append('<li class="mainphoto-box"><img alt="メイン写真:' + i + '" class="mainphoto-image" src="' + data.photo_catch[i] + '"></li>');
-    }
+    $('#pillow-word').text(data.pillow_word);
+    $('#mainphoto-view').append('<li class="mainphoto-box"><img alt="メイン写真" class="mainphoto-image" src="/img/' + data.id + '/catch.jpg"></li>');
   })
   .fail(function () {
     console.log('fail');
