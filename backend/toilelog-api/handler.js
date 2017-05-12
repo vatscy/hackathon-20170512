98328b593fd1b246f03057e7cc23fb4fdd19444f
Vatscy <vatscy8241@gmail.com>
2017-05-12 12:17:28 +0900

    微修正

--- backend/toilelog-api/handler.js
+++ backend/toilelog-api/handler.js
@@ -5,14 +5,13 @@ var dynamo = new AWS.DynamoDB.DocumentClient();
 var tableName = "toilets";
 var toilet_id = "123456789";
 
-
-module.exports.hello = (event, context, callback) => {
+module.exports.toilet = (event, context, callback) => {
 
   //Request
   var param = {
-    "tableName": tableName
-    "Key"{
-      "toilet_id": toilet_id
+    "TableName": tableName,
+    "Key": {
+      "id": toilet_id
     }
   };
 
@@ -26,24 +25,24 @@ module.exports.hello = (event, context, callback) => {
   //
   // callback(null, response);
 
+  const response = {};
 
   dynamo.get(param, function(err, data) {
     if (data.Items){
       response.statusCode = 200;
       response.body = JSON.stringify({
-        "place": "場所"
-        "photo": "写真"
-        "evaluation": "評価"
+        "place": "場所",
+        "photo": "写真",
+        "evaluation": "評価",
         "information": "情報"
-      })
+      });
     } else {
       response.statusCode = 400;
       response.body = JSON.stringify({
           "message": "対象のトイレは存在しません。"
       });
-      callback(null, response);
-      return;
     }
+    callback(null, response);
 });
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
