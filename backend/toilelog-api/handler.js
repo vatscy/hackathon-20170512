f5a4bec19c0f394eca77baed9624dd0affd6a1ee
fuwamaki0324 <variety.fuwa.3@gmail.com>
2017-05-12 11:52:18 +0900

    サンプルで

--- backend/toilelog-api/handler.js
+++ backend/toilelog-api/handler.js
@@ -1,15 +1,50 @@
 'use strict';
 
+var AWS = require("aws-sdk");
+var dynamo = new AWS.DynamoDB.DocumentClient();
+var tableName = "toilets";
+var toilet_id = "123456789";
+
+
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+
+  //Request
+  var param = {
+    "tableName": tableName
+    "Key"{
+      "toilet_id": toilet_id
+    }
   };
 
-  callback(null, response);
+  // const response = {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     message: 'Go Serverless v1.0! Your function executed successfully!',
+  //     input: event,
+  //   }),
+  // };
+  //
+  // callback(null, response);
+
+
+  dynamo.get(param, function(err, data) {
+    if (data.Items){
+      response.statusCode = 200;
+      response.body = JSON.stringify({
+        "place": "場所"
+        "photo": "写真"
+        "evaluation": "評価"
+        "information": "情報"
+      })
+    } else {
+      response.statusCode = 400;
+      response.body = JSON.stringify({
+          "message": "対象のトイレは存在しません。"
+      });
+      callback(null, response);
+      return;
+    }
+});
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
