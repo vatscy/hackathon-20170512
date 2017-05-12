16962d84400de92183d338fe2b15d5ae12d92cf6
Vatscy <vatscy8241@gmail.com>
2017-05-12 17:02:33 +0900

    データバインディング一旦完了

--- frontend/scripts/detail.js
+++ frontend/scripts/detail.js
@@ -12,6 +12,21 @@ $(function() {
     $('#toilet-name').text(data.name);
     $('#pillow-word').text(data.pillow_word);
     $('#mainphoto-view').append('<li class="mainphoto-box"><img alt="メイン写真" class="mainphoto-image" src="/img/' + data.id + '/catch.jpg"></li>');
+    $('#toilet-address').text(data.address);
+
+    var info = data.information;
+    var $infoBasic = $('#toilet-info-basic');
+    function tf(isPresent) {
+      return isPresent ? 'あり': '-';
+    }
+    $infoBasic.append('<tr><th>営業時間</th><td><p>'+ info.time +'</p></td></tr>');
+    $infoBasic.append('<tr><th>洋式トイレ</th><td><p>'+ tf(info.style_western) + '</p></td></tr>');
+    $infoBasic.append('<tr><th>和式トイレ</th><td><p>'+ tf(info.style_japan) + '</p></td></tr>');
+    $infoBasic.append('<tr><th>ウォシュレット</th><td><p>'+ tf(info.washlet) + '</p></td></tr>');
+    $infoBasic.append('<tr><th>多目的トイレ</th><td><p>'+ tf(info.multipurpose) + '</p></td></tr>');
+    $infoBasic.append('<tr><th>パウダールーム</th><td><p>'+ tf(info.powder_room) + '</p></td></tr>');
+    $infoBasic.append('<tr><th>おむつ交換設備</th><td><p>'+ tf(info.diaper_change_space) + '</p></td></tr>');
+    $infoBasic.append('<tr><th>車椅子対応</th><td><p>'+ tf(info.wheelchair) + '</p></td></tr>');
   })
   .fail(function () {
     console.log('fail');
