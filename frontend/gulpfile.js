68aab6937a551da574d1736989cb299d131d3590
Vatscy <vatscy8241@gmail.com>
2017-05-12 12:52:11 +0900

    add s3 deploy task

---
+++ frontend/gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const gulp = require('gulp');
+const sequence = require('run-sequence');
+
+gulp.task('upload-s3', (done) => {
+  const s3 = require('gulp-s3-upload')({
+    useIAM: true
+  });
+  const settings = {
+    Bucket: 'toilet.hc-mti.com',
+    ACL: 'public-read'
+  };
+  return gulp.src(['*', '!gulpfile.js', '!package.json'])
+    .pipe(s3(settings), done);
+});
+
+gulp.task('deploy', (done) =>
+  sequence('upload-s3', done)
+);
